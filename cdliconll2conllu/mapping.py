1ada3087af042f3f0811860ff01da493926d5e97
asagar06 <anooshasagar@gmail.com>
2018-03-22 12:30:23 -0700

    filename bug

--- cdliconll2conllu/mapping.py
+++ cdliconll2conllu/mapping.py
@@ -2,13 +2,13 @@ import json
 import pkg_resources
 
 resource_package = __name__  # Could be any module/package name
-mappingFile = '/'.join('mapping.json')
+#mappingFile = '/'.join(('mapping.json')
 
 class mapping:
     def __init__(self):
 
         #open json file containing mapping information and load it in a python object
-        filepath = pkg_resources.resource_filename(__name__, mappingFile)
+        filepath = pkg_resources.resource_filename(__name__, 'mapping.json')
         self.jsonFile = filepath
         print(self.jsonFile)
         self.jsonData = json.load(open(self.jsonFile))
@@ -42,3 +42,5 @@ class mapping:
 
     def createMultiList(self):
         return self.jsonData['multipleFeatureEntries']
+
+mapping()
