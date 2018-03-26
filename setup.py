fa25189cdb8d2478ca666d772b6dda3d93caefb6
asagar06 <anooshasagar@gmail.com>
2018-03-26 13:49:38 -0700

    updated version number
    added provision for exception handling in case of incorrect XPOSTAG

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ dependencies = ['click']
 
 setup(
     name='cdliconll2conllu',
-    version='2.0.3',
+    version='2.0.4',
     url='https://github.com/cdli-gh/CDLI-CoNLL-to-CoNLLU-Converter',
     license='BSD',
     author='Anoosha Sagar',
